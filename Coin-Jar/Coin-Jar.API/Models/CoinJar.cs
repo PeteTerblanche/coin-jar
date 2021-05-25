4aac1a04defab06dd2c12a828469274951943126
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2021-05-25 06:20:47 +0200

    Add the CoinJar functionality with backing tests

--- Coin-Jar/Coin-Jar.API/Models/CoinJar.cs
+++ Coin-Jar/Coin-Jar.API/Models/CoinJar.cs
@@ -1,22 +1,29 @@
-﻿using Coin_Jar.API.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Coin_Jar.API.Interfaces;
 
 namespace Coin_Jar.API.Models
 {
     public class CoinJar :ICoinJar
     {
+        private const decimal MAX_FLUID_OUNCE = 42;
+        public List<ICoin> Coins { get; } = new List<ICoin>();
         public void AddCoin(ICoin coin)
         {
+            var tempCoins = new List<ICoin>(Coins) {coin};
+            var currentVolume = tempCoins.Sum(c => c.Volume);
+            if (currentVolume > MAX_FLUID_OUNCE)
+            {
+                var totalVolume = GetTotalVolume();
+                throw new Exception($"Current volume is {totalVolume} ounces. Maximum of {MAX_FLUID_OUNCE} ounces accepted.");
+            }
 
+            Coins.Add(coin);
         }
 
-        public decimal GetTotalAmount()
-        {
-            return 0;
-        }
-
-        public void Reset()
-        {
-
-        }
+        private decimal GetTotalVolume() => Coins.Sum(coin => coin.Volume);
+        public decimal GetTotalAmount() => Coins.Sum(coin => coin.Amount);
+        public void Reset() => Coins.Clear();
     }
 }
--- Coin-Jar/Coin-Jar.Tests/Models/CoinJarTests.cs
+++ Coin-Jar/Coin-Jar.Tests/Models/CoinJarTests.cs
@@ -1,10 +1,58 @@
-﻿using NUnit.Framework;
+﻿using Coin_Jar.API.Models;
+using NUnit.Framework;
 
 namespace Coin_Jar.Tests.Models
 {
     [TestFixture]
     public class CoinJarTests
     {
-        
+        [Test]
+        public void Given_AddCoin_When_Coin_Validation_Pass_Then_Expect_Coin_Added()
+        {
+            var coinJar = new CoinJar();
+            coinJar.AddCoin(new Coin {Amount = 1, Volume = 1});
+
+            Assert.IsNotNull(coinJar.Coins);
+            Assert.That(() => coinJar.Coins.Count == 1);
+            Assert.That(() => coinJar.Coins[0].Amount == 1);
+            Assert.That(() => coinJar.Coins[0].Volume == 1);
+        }
+
+        [Test]
+        public void Given_AddCoin_When_MaxVolume_Exceeded_Then_Expect_Exception()
+        {
+            const decimal total = 42.01m;
+            var errMsg = $"Current volume is {total} ounces. Maximum of 42 ounces accepted.";
+
+            var coin = new Coin {Amount = 500m, Volume = 42.01m};
+            var coinJar = new CoinJar();
+            coinJar.Coins.Add(coin);
+
+            Assert.That(() => coinJar.AddCoin(coin), Throws.Exception.With.Message.EqualTo(errMsg));
+        }
+
+        [Test]
+        public void Given_AddCoin_When_GetTotalAmount_Then_Expect_Total_Returned()
+        {
+            var coinJar = new CoinJar();
+            coinJar.AddCoin(new Coin {Amount = 15, Volume = 15});
+
+            Assert.That(() => coinJar.Coins != null);
+            Assert.That(() => coinJar.GetTotalAmount() == 15);
+        }
+
+        [Test]
+        public void CoinJar_AddCoin_Reset_Then_Expect_Coins_Cleared()
+        {
+            var coinJar = new CoinJar();
+            coinJar.AddCoin(new Coin {Amount = 15, Volume = 15});
+
+            Assert.That(() => coinJar.Coins != null);
+            Assert.That(() => coinJar.GetTotalAmount() == 15);
+
+            coinJar.Reset();
+
+            Assert.That(() => coinJar.GetTotalAmount() == 0);
+        }
     }
 }
